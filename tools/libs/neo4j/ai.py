1c7c3ef4a225ee8590830f5cada4c9de17154ece
Philippe Dessauw <philippe.dessauw@nist.gov>
2019-05-29 17:41:30 -0400

    fix: factorize code for starting neo4j container

--- tools/libs/neo4j/ai.py
+++ tools/libs/neo4j/ai.py
@@ -1,10 +1,11 @@
 import docker
 from docker.errors import APIError
 
-from settings import LOGGER
+from settings import LOGGER, NEO4J_V3_MEMORY
 from utils.containers import wait_log_display
 from utils.rand import get_rand_string
 from utils.statistics import get_time
+
 
 START_STRING = "Remote interface available"
 
@@ -36,76 +37,25 @@ COMMANDS = [
 
 
 def import_csv_files(db_path, import_dir):
-    docker_cli = docker.from_env()
-    cname = "neo4j-v3-%s" % get_rand_string(5, special=False)
-    LOGGER.info("Starting %s..." % cname)
+    neo4j3_container, cname = start_container(db_path, import_dir=import_dir)
 
-    container = docker_cli.containers.run(
-        "neo4j-ai:latest",
-        name=cname,
-        environment={
-            "NEO4J_dbms_memory_pagecache_size": "2G",
-            "NEO4J_dbms_memory_heap_max__size": "2G",
-            "NEO4J_dbms_allow__upgrade": "true",
-            "NEO4J_dbms_shell_enabled": "true",
-            "NEO4J_AUTH": "none"
-        },
-        ports={
-            "7474": "7474",
-            "7687": "7687",
-        },
-        volumes={
-            db_path: {"bind": "/data/databases/neo4j_v3.db", "mode": "rw"},
-            import_dir: {"bind": "/var/lib/neo4j/import", "mode": "rw"}
-        },
-        detach=True
-    )
-
-    wait_log_display(container, START_STRING)
-
-    LOGGER.info("%s fully started." % cname)
-
-    container.exec_run(
+    neo4j3_container.exec_run(
         """
         ./bin/neo4j-admin import --database=neo4j_v3.db --delimiter='TAB'
             --nodes=./import/nodes.csv --relationships=./import/edges.csv    
         """
     )
 
-    LOGGER.info("%s fully imported." % cname)
-    container.stop()
+    LOGGER.info("CSV file imported.")
+    neo4j3_container.stop()
 
     return cname
 
 
 def enhance_markup(db_path):
-    docker_cli = docker.from_env()
-    cname = "neo4j-v3-%s" % get_rand_string(5, special=False)
-    LOGGER.info("Starting %s..." % cname)
+    neo4j3_container, cname = start_container(db_path)
 
-    neo4j3_container = docker_cli.containers.run(
-        "neo4j-ai:latest",
-        name=cname,
-        environment={
-            "NEO4J_dbms_memory_pagecache_size": "2G",
-            "NEO4J_dbms_memory_heap_max__size": "2G",
-            "NEO4J_dbms_allow__upgrade": "true",
-            "NEO4J_dbms_shell_enabled": "true",
-            "NEO4J_AUTH": "none"
-        },
-        ports={
-            "7474": "7474",
-            "7687": "7687",
-        },
-        volumes={
-            db_path: {"bind": "/data/databases/graph.db", "mode": "rw"}
-        },
-        detach=True
-    )
-
-    wait_log_display(neo4j3_container, START_STRING)
-
-    LOGGER.info("%s started. Running commands..." % cname)
+    LOGGER.info("Running commands...")
     for cmd in COMMANDS:
         try:
             start = get_time()
@@ -129,17 +79,27 @@ def enhance_markup(db_path):
     return cname
 
 
-def start_container(db_path, stop_after_execution=True):
+def start_container(db_path, import_dir=None, stop_after_execution=False):
     docker_cli = docker.from_env()
     cname = "neo4j-v3-%s" % get_rand_string(5, special=False)
     LOGGER.info("Starting %s..." % cname)
+
+    neo4j3_container_volumes = {
+        db_path: {"bind": "/data/databases/graph.db", "mode": "rw"}
+    }
+
+    if import_dir is not None:
+        neo4j3_container_volumes[import_dir] = {
+            "bind": "/var/lib/neo4j/import",
+            "mode": "rw"
+        }
 
     neo4j3_container = docker_cli.containers.run(
         "neo4j-ai:latest",
         name=cname,
         environment={
-            "NEO4J_dbms_memory_pagecache_size": "2G",
-            "NEO4J_dbms_memory_heap_max__size": "2G",
+            "NEO4J_dbms_memory_pagecache_size": NEO4J_V3_MEMORY,
+            "NEO4J_dbms_memory_heap_max__size": NEO4J_V3_MEMORY,
             "NEO4J_dbms_allow__upgrade": "true",
             "NEO4J_dbms_shell_enabled": "true",
             "NEO4J_AUTH": "none"
@@ -148,17 +108,15 @@ def start_container(db_path, stop_after_execution=True):
             "7474": "7474",
             "7687": "7687",
         },
-        volumes={
-            db_path: {"bind": "/data/databases/graph.db", "mode": "rw"}
-        },
+        volumes=neo4j3_container_volumes,
         detach=True
     )
 
     wait_log_display(neo4j3_container, START_STRING)
 
-    LOGGER.info("%s fully started." % cname)
+    LOGGER.info("%s started." % cname)
 
     if stop_after_execution:
         neo4j3_container.stop()
 
-    return cname
+    return neo4j3_container, cname
