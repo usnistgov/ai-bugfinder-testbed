110a365f03d028b40aa56d2f9691e9e2c6d9bd4a
Philippe Dessauw <philippe.dessauw@nist.gov>
2020-01-15 11:45:12 -0500

    chore: remove old code

--- tools/libs/ast/common.py
+++
@@ -1,8 +0,0 @@
-
-def set_ast_prop(ast_update_dict):
-    command = """
-        UNWIND %s as data
-        MERGE (n) WHERE id(n) = data.id
-        SET n.ast = data.ast
-    """ % ast_update_dict
-
