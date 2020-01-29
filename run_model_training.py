68ed6585a1eeadf63cac9594076eb91dd48d340f
Philippe Dessauw <philippe.dessauw@nist.gov>
2020-01-28 19:10:07 -0500

    feat: add model training script

---
+++ run_model_training.py
@@ -0,0 +1,33 @@
+""" Script to run feature extraction
+"""
+import argparse
+
+from tools.dataset import CWEClassificationDataset as Dataset
+from tools.models.dnn_classifier import DNNClassifierTraining
+from tools.models.linear_classifier import LinearClassifierTraining
+from tools.utils.processing import is_operation_valid
+
+if __name__ == "__main__":
+    options = {  # Dictionary linking input arguments to processing classes
+        "linear_classifier": LinearClassifierTraining,
+        "deep_neural_network": DNNClassifierTraining,
+    }
+
+    # Setup the argument parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument("dataset_path", help="path to the dataset to clean")
+    parser.add_argument("--model", "-m", choices=options.keys(), required=True,
+                        help="path to the dataset to clean")
+
+    args = parser.parse_args()
+
+    # Instantiate dataset class and run joern processing
+    dataset = Dataset(args.dataset_path)
+
+    operation_class = options[args.model]
+
+    is_operation_valid(operation_class)
+
+    dataset.queue_operation(operation_class)
+
+    dataset.process()
