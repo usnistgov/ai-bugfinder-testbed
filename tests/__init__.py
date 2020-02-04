e31e6e855913e2ff45056748b954e89313bc2bb6
Philippe Dessauw <philippe.dessauw@nist.gov>
2020-02-04 11:42:38 -0500

    feat: add unit tests for utils/containers.py

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,2 +1,10 @@
 """ Tests for bugfinder package
 """
+
+
+def mock_return_fn_args_as_dict(*args, **kwargs):
+    kwargs.update({
+        "arg%d" % i: args[i] for i in range(len(args))
+    })
+
+    return kwargs
