aaba1c8a2b7a9e065e408e09c91097a4318eee1b
Philippe Dessauw <philippe.dessauw@nist.gov>
2020-02-05 18:52:10 -0500

    feat: add util rand tests

--- bugfinder/utils/rand.py
+++ bugfinder/utils/rand.py
@@ -3,12 +3,15 @@
 import string
 from random import choice
 
+NB_CHARS = "1234567890"
+SP_CHARS = "!@#$%^&*()_+-="
+
 
 def get_rand_string(length, lower=True, upper=True, numbers=True, special=True):
     lc = string.ascii_lowercase
     uc = string.ascii_uppercase
-    nb = "1234567890"
-    sp = "!@#$%^&*()_+-="
+    nb = NB_CHARS
+    sp = SP_CHARS
 
     chars = ""
     chars += lc if lower else ""
--- tests/utils/rand/test_unit.py
+++ tests/utils/rand/test_unit.py
@@ -1,23 +1,38 @@
 """ Utilities using random generators
 """
 import unittest
+import string
+from bugfinder.utils.rand import get_rand_string, NB_CHARS, SP_CHARS
 
 
 class TestGetRandString(unittest.TestCase):
     def test_string_length_is_correct(self):
-        pass
+        self.assertEqual(len(get_rand_string(5)), 5)
 
     def test_all_false_throws_error(self):
-        pass
+        with self.assertRaises(IndexError):
+            get_rand_string(5, False, False, False, False)
 
     def test_no_lower_returns_correct_string(self):
-        pass
+        self.assertNotIn(
+            string.ascii_lowercase,
+            get_rand_string(5, False, True, True, True)
+        )
 
     def test_no_upper_returns_correct_string(self):
-        pass
+        self.assertNotIn(
+            string.ascii_uppercase,
+            get_rand_string(5, True, False, True, True)
+        )
 
     def test_no_numbers_returns_correct_string(self):
-        pass
+        self.assertNotIn(
+            NB_CHARS,
+            get_rand_string(5, True, True, False, True)
+        )
 
     def test_no_specials_returns_correct_string(self):
-        pass
+        self.assertNotIn(
+            SP_CHARS,
+            get_rand_string(5, True, True, True, False)
+        )
