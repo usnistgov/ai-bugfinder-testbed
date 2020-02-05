0a4f3c42f40d9a4128dcff359610b7a6128128a2
Philippe Dessauw <philippe.dessauw@nist.gov>
2020-02-05 18:53:31 -0500

    feat: add util stats tests

--- tests/utils/statistics/test_unit.py
+++ tests/utils/statistics/test_unit.py
@@ -1,8 +1,15 @@
 """ Utilities for statistic purposes.
 """
 import unittest
+from time import sleep
+
+from bugfinder.utils.statistics import get_time
 
 
 class TestGetTime(unittest.TestCase):
     def test_time_is_in_millisecond(self):
-        pass
+        start_time = get_time()
+        sleep(1)
+        total_time = get_time() - start_time
+
+        self.assertTrue(1500 > total_time > 500)
