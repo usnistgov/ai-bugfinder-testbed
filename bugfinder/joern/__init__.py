0a20d291eb226f66ab3b1845129fd15b16351a3a
Philippe Dessauw <philippe.dessauw@nist.gov>
2022-02-11 11:25:41 -0500

    chore: cleanup bugfinder.joern package

--- bugfinder/joern/__init__.py
+++ bugfinder/joern/__init__.py
@@ -1,13 +1,19 @@
+""" Module containing abstract classes for any Joern processing.
+"""
 from abc import abstractmethod
 
 from bugfinder.dataset.processing import DatasetProcessingWithContainer
 
 
 class JoernDefaultDatasetProcessing(DatasetProcessingWithContainer):
+    """Abstract Joern processing class"""
+
     def configure_container(self):
+        """Setup the properties of the container"""
         self.volumes = {self.dataset.path: "/code"}
         self.detach = False
 
     @abstractmethod
     def send_commands(self):
+        """Send the commands. Must be implemented by subclasses."""
         raise NotImplementedError("Method 'send_commands' not implemented.")
