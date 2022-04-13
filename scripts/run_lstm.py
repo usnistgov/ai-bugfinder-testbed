61ced41052f5d9fbe39bf183a656ac93a218215c
Philippe Dessauw <philippe.dessauw@nist.gov>
2022-04-13 17:47:17 -0400

    chore: move lstm utils in lstm training script

--- scripts/run_lstm.py
+++ scripts/run_lstm.py
@@ -1,19 +1,46 @@
 """
 LSTM implementation
 """
-
 from os.path import dirname, join
+
 import sys
 
 sys.path.append(join(dirname(__file__), ".."))
 
 import argparse
-import logging
-import os
-
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
 from tensorflow import keras
+
 from bugfinder.settings import LOGGER
-from bugfinder.dataset.processing.lstm_utils import process_csv
+
+
+def process_csv(input_file, test_data_size=0.33, seed=42):
+    LOGGER.info("Processing feature file...")
+
+    dataframe = pd.read_csv(input_file)
+
+    LOGGER.info("Shape of the dataframe: %s" % (str(dataframe.shape)))
+
+    y = (dataframe["result"].astype(int)).to_numpy()
+
+    dataframe = dataframe.drop(["name", "result"], axis=1)
+
+    x = dataframe.to_numpy()
+    X = np.reshape(x, (x.shape[0], x.shape[1], 1))
+
+    x_train, y_train, x_test, y_test = train_test_split(
+        X, y, test_size=test_data_size, random_state=seed
+    )
+
+    LOGGER.info("Training and validation sets created.")
+    LOGGER.info(
+        "%d instances will be used for training and %d instances for validation"
+        % (x_train.shape[0], x_test.shape[0])
+    )
+
+    return x_train, x_test, y_train, y_test
 
 
 # TODO: integrate this function in the LSTMClassifierTraining class
