29d733fb9ce772d59b4c0d71c1be1b98916f21e9
Philippe Dessauw <philippe.dessauw@nist.gov>
2022-04-13 11:55:29 -0400

    fix: remove unused ASTSetExporter class

--- tests/ast/test_unit.py
+++ tests/ast/test_unit.py
@@ -1,11 +1,9 @@
-import pickle
-from os.path import join
 from unittest import TestCase
-from unittest.mock import patch, mock_open, Mock
 
-from bugfinder.ast import AbstractASTMarkup, ASTSetExporter
+from unittest.mock import patch, Mock
+
+from bugfinder.ast import AbstractASTMarkup
 from bugfinder.dataset import CodeWeaknessClassificationDataset
-from bugfinder.settings import ROOT_DIR
 from tests import patch_paths
 
 
@@ -60,104 +58,3 @@ class TestAbstractASTMarkupSendCommands(TestCase):
 
         self.dataset_processing.send_commands()
         self.assertTrue(mock_neo4j_db.run.called)
-
-
-class TestASTSetExporterExecute(TestCase):
-    def setUp(self) -> None:
-        patch_paths(
-            self, ["bugfinder.ast.LOGGER", "bugfinder.dataset.processing.LOGGER"]
-        )
-
-        self.dataset_processing = ASTSetExporter(None)
-
-    @patch("bugfinder.neo4j.Neo4J3Processing.execute")
-    def test_ast_file_is_setup(self, mock_execute):
-        mock_execute.return_value = None
-        expected_result = "mock_ast_file"
-        self.dataset_processing.execute(ast_file=expected_result)
-        self.assertEqual(self.dataset_processing.ast_file, expected_result)
-
-    @patch("bugfinder.neo4j.Neo4J3Processing.execute")
-    def test_execute_called(self, mock_execute):
-        self.dataset_processing.execute()
-        self.assertTrue(mock_execute.called)
-
-
-class TestASTSetExporterConfigureContainer(TestCase):
-    def setUp(self) -> None:
-        patch_paths(
-            self, ["bugfinder.ast.LOGGER", "bugfinder.dataset.processing.LOGGER"]
-        )
-
-        self.dataset_processing = ASTSetExporter(None)
-
-    @patch("bugfinder.neo4j.Neo4J3Processing.configure_container")
-    def test_configure_container_called(self, mock_configure_container):
-        self.dataset_processing.configure_container()
-        self.assertTrue(mock_configure_container.called)
-
-    @patch("bugfinder.neo4j.Neo4J3Processing.configure_container")
-    def test_container_name_is_setup(self, mock_configure_container):
-        mock_configure_container.return_value = None
-        self.dataset_processing.configure_container()
-        self.assertEqual(self.dataset_processing.container_name, "ast-set-generator")
-
-
-class TestASTSetExporterSendCommands(TestCase):
-    def setUp(self) -> None:
-        patch_paths(
-            self,
-            [
-                "builtins.open",
-                "bugfinder.ast.LOGGER",
-                "bugfinder.dataset.processing.LOGGER",
-            ],
-        )
-
-        self.dataset_processing = ASTSetExporter(None)
-        self.dataset_processing.ast_file = "mock_file"
-
-    @patch("bugfinder.neo4j.Neo4J3Processing.neo4j_db")
-    @patch("bugfinder.neo4j.Neo4J3Processing.send_commands")
-    def test_send_commands_called(self, mock_send_commands, mock_neo4j_db):
-        mock_neo4j_db.return_value = None
-        self.dataset_processing.send_commands()
-        self.assertTrue(mock_send_commands.called)
-
-    @patch("bugfinder.neo4j.Neo4J3Processing.neo4j_db")
-    @patch("bugfinder.ast.load")
-    @patch("bugfinder.ast.exists")
-    @patch("bugfinder.neo4j.Neo4J3Processing.send_commands")
-    def test_ast_file_reloaded_if_exists(
-        self, mock_send_commands, mock_exists, mock_load, mock_neo4j_db
-    ):
-        mock_send_commands.return_value = None
-        mock_exists.return_value = True
-        mock_load.return_value = []
-        mock_neo4j_db.return_value = None
-
-        with patch("builtins.open", mock_open(read_data=b"data")) as mock_file:
-            self.dataset_processing.send_commands()
-            mock_file.assert_any_call(
-                join(ROOT_DIR, self.dataset_processing.ast_file), "rb"
-            )
-
-            self.assertEqual(mock_file.call_count, 2)
-
-    @patch("bugfinder.neo4j.Neo4J3Processing.neo4j_db")
-    @patch("bugfinder.ast.exists")
-    @patch("bugfinder.neo4j.Neo4J3Processing.send_commands")
-    def test_ast_list_empty_if_file_does_not_exists(
-        self, mock_send_commands, mock_exists, mock_neo4j_db
-    ):
-        mock_send_commands.return_value = None
-        mock_exists.return_value = False
-        mock_neo4j_db.return_value = None
-
-        with patch("builtins.open", mock_open(read_data=b"data")) as mock_file:
-            self.dataset_processing.send_commands()
-            mock_file.assert_any_call(
-                join(ROOT_DIR, self.dataset_processing.ast_file), "wb"
-            )
-
-            self.assertEqual(mock_file.call_count, 1)
